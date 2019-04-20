912ead605ac0ff9dd7261fad3db64565dc4991a6
manimigue <manimigue0701@gmail.com>
2019-04-21 07:33:09 +0900

    added israel as

--- src/concert/components.js
+++ src/concert/components.js
@@ -1,14 +1,6 @@
-import { default as israel } from "./articles/israel.jsx";
+
 
 const concert =
-[
-  {
-    title: "Test",
-    url: "israel",
-    date: "2019-04-21",
-    author: "三石ミゲール",
-    component: israel
-  }
-]
+[]
 
 export default concert
