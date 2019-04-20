e98ddfdf5b15b8773347f377ac2e2753416e9fc3
manimigue <manimigue0701@gmail.com>
2019-04-21 07:08:04 +0900

    added israel as

--- src/concert/components.js
+++ src/concert/components.js
@@ -1,15 +1,6 @@
-import { default as ai } from "./articles/ai.jsx";
-import { default as israel } from "./articles/israel.jsx";
+
 
 const concert =
-[
-  {
-    title: "AI Boot Camp",
-    url: "ai",
-    date: "2019-04-20",
-    author: "三石ミゲール",
-    component: ai
-  }
-]
+[]
 
 export default concert
