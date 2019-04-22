c8dde439457e4635c877826b23dbc492e67f6878
manimigue <manimigue0701@gmail.com>
2019-04-23 01:54:16 +0900

    added settings as 初期設定

--- src/news/components.js
+++ src/news/components.js
@@ -1,8 +1,16 @@
 import { default as ai } from "./articles/ai.jsx";
 import { default as israel } from "./articles/israel.jsx";
+import { default as settings } from "./articles/settings.jsx";
 
 const news =
 [
+  {
+    title: "初期設定",
+    url: "settings",
+    date: "2019-04-23",
+    author: "三石ミゲール",
+    component: settings
+  },
   {
     title: "Israel Report",
     url: "israel",
