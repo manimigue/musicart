fe80b6ec4e7722f17e3e75d86c0f253e8216efd6
manimigue <manimigue0701@gmail.com>
2019-04-24 20:01:16 +0900

    added typora as Typoraの使い方

--- src/news/components.js
+++ src/news/components.js
@@ -1,9 +1,17 @@
 import { default as ai } from "./articles/ai.jsx";
 import { default as israel } from "./articles/israel.jsx";
 import { default as settings } from "./articles/settings.jsx";
+import { default as typora } from "./articles/typora.jsx";
 
 const news =
 [
+  {
+    title: "Typoraの使い方",
+    url: "typora",
+    date: "2019-04-24",
+    author: "三石ミゲール",
+    component: typora
+  },
   {
     title: "初期設定",
     url: "settings",
