7e77c0c1e379d8cd3ba962a000b88994c20ba938
manimigue <manimigue0701@gmail.com>
2019-04-29 18:22:17 +0900

    removed test3

--- src/news/components.js
+++ src/news/components.js
@@ -1,16 +1,8 @@
 import { default as israel } from "./articles/israel.jsx";
 import { default as settings } from "./articles/settings.jsx";
-import { default as test3 } from "./articles/test3.jsx";
 
 const news =
 [
-  {
-    title: "Test",
-    url: "test3",
-    date: "2019-04-29",
-    author: "三石ミゲール",
-    component: test3
-  },
   {
     title: "初期設定",
     url: "settings",
