e6d6e46b4ff44c1950474c5acb5aa19d874c543a
manimigue <manimigue0701@gmail.com>
2019-04-29 18:26:08 +0900

    added test2 as Test2

--- src/news/components.js
+++ src/news/components.js
@@ -1,9 +1,17 @@
 import { default as israel } from "./articles/israel.jsx";
 import { default as settings } from "./articles/settings.jsx";
 import { default as test } from "./articles/test.jsx";
+import { default as test2 } from "./articles/test2.jsx";
 
 const news =
 [
+  {
+    title: "Test2",
+    url: "test2",
+    date: "2019-04-29",
+    author: "三石ミゲール",
+    component: test2
+  },
   {
     title: "Test",
     url: "test",
