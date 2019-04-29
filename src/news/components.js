4954ed20d7c7d0f08de1de0d7aa641b84533ec6d
manimigue <manimigue0701@gmail.com>
2019-04-29 17:50:55 +0900

    added test3 as Test3

--- src/news/components.js
+++ src/news/components.js
@@ -1,16 +1,8 @@
 import { default as israel } from "./articles/israel.jsx";
 import { default as settings } from "./articles/settings.jsx";
-import { default as typora } from "./articles/typora.jsx";
 
 const news =
 [
-  {
-    title: "Typoraの使い方",
-    url: "typora",
-    date: "2019-04-24",
-    author: "三石ミゲール",
-    component: typora
-  },
   {
     title: "初期設定",
     url: "settings",
