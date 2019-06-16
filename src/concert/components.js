f11caa27091a58b53244c5e11f1b0f0214f5b84f
manimigue <manimigue0701@gmail.com>
2019-06-16 19:41:21 +0900

    added Danse_macabre as 交響詩「死の舞踏」 Danse macabre 作品40 / サン=サーンスSaint-Saëns, Camille

--- src/concert/components.js
+++ src/concert/components.js
@@ -1,3 +1,4 @@
+import { default as Danse_macabre } from "./articles/Danse_macabre.jsx";
 import { default as Les_Biches } from "./articles/Les_Biches.jsx";
 import { default as Symphonie_Fantastique } from "./articles/Symphonie_Fantastique.jsx";
 import { default as ai } from "./articles/ai.jsx";
@@ -9,6 +10,13 @@ import { default as typora } from "./articles/typora.jsx";
 
 const concert =
 [
+  {
+    title: "交響詩「死の舞踏」 Danse macabre 作品40 / サン=サーンスSaint-Saëns, Camille",
+    url: "Danse_macabre",
+    date: "2019-06-16",
+    author: "三石ミゲール",
+    component: Danse_macabre
+  },
   {
     title: "バレエ組曲「牝鹿」 Les Biches Suite d’Orchestre  / プーランクPoulenc, Francis",
     url: "Les_Biches",
