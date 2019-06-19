5c715ac0c75f77e16eadb1aebafa6816cea81652
manimigue <manimigue0701@gmail.com>
2019-06-19 22:25:17 +0900

    added 20190619news2 as チケット予約開始（ホームページ予約第1次）

--- src/news/articles/20190619news1.jsx
+++ src/news/articles/20190619news1.jsx
@@ -1,14 +1,35 @@
 
 import React, { Component } from 'react'
 import MarkdownRender from "@nteract/markdown";
+import Link from '../../components/Link'
 
-class 20190619news1 extends Component {
+class C20190619news1 extends Component {
   componentDidMount() {
     window.scrollTo(0, 0)
   }
   render(){
     const source = `
+# ホームページ開設のお知らせ
 
+### 2019.06.20
+
+Orchestra Mµsicart（オーケストラ・ミュージカルト）公式ホームページを開設しました！
+
+当団は多くの人がクラシックコンサートに足を運びたくなるような、新しいアプローチを目指して2018年に発足しました。これからこのホームページを通じて、当団の活動や、コンサート情報など発信していきたいと思っております。
+
+
+
+早速ですが、第1回目となるコンサート「Orchestra Mµsicart 光と魔法のコンサート in 森のホール21」を2019 年11月3日（日祝）に森のホール21大ホールで予定しております。詳しくは [Concert](https://manimigue.github.io/musicart/concert/light_and_magic)をご覧ください。
+
+また、団員・スタッフを引き続き募集しております。こちらに関しては[Join Us](https://manimigue.github.io/musicart/members)をご覧ください。
+
+
+
+皆様にお会いできる日を楽しみにしております。
+
+
+
+Marketing Division E.Y.
 `
     return(
       <MarkdownRender className='article concert' source={source} escapeHtml={false}/>
@@ -16,4 +37,4 @@ class 20190619news1 extends Component {
   }
 }
 
-export default 20190619news1;
+export default C20190619news1;
--- src/news/components.js
+++ src/news/components.js
@@ -1,33 +1,27 @@
-import { default as release } from "./articles/release.jsx";
+
 
 const news =
 [
   {
-    title: "ホームページリリース",
-    url: "release",
-    date: "2019-05-02",
+    title: "チケット予約開始（ホームページ予約第1次）",
+    url: "20190619news2",
+    date: "2019-06-19",
     author: "三石ミゲール",
-    component: release
-  }
-]
-
-import default news
-
-const news =
-[
+    component: C20190619news2
+  },
   {
     title: "ホームページ開設のお知らせ",
     url: "20190619news1",
     date: "2019-06-19",
     author: "三石ミゲール",
-    component: 20190619news1
+    component: C20190619news1
   },
   {
     title: "ホームページリリース",
     url: "release",
     date: "2019-05-02",
     author: "三石ミゲール",
-    component: release
+    component: Crelease
   }
 ]
 
