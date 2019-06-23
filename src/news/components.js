354d3047381ad49de386236f3971c3372a7ddf24
manimigue <manimigue0701@gmail.com>
2019-06-23 23:19:15 +0900

    added apptest as 非線形

--- src/news/components.js
+++ src/news/components.js
@@ -3,7 +3,6 @@ import { default as news_20190619news2 } from "./articles/news_20190619news2.jsx
 import { default as news_20190619news3 } from "./articles/news_20190619news3.jsx";
 import { default as news_20190619news4 } from "./articles/news_20190619news4.jsx";
 import { default as news_apptest } from "./articles/news_apptest.jsx";
-import { default as news_atest } from "./articles/news_atest.jsx";
 
 const news =
 [
@@ -13,13 +12,6 @@ const news =
     date: "2019-06-23",
     author: "三石ミゲール",
     component: news_apptest
-  },
-  {
-    title: "A",
-    url: "atest",
-    date: "2019-06-23",
-    author: "三石ミゲール",
-    component: news_atest
   },
   {
     title: "第2回定期公演決定のお知らせ",
