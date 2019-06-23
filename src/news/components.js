478f318a641e6e25ca0d35d9a7b46b3cafa150c5
manimigue <manimigue0701@gmail.com>
2019-06-23 22:30:49 +0900

    added test as Test

--- src/news/components.js
+++ src/news/components.js
@@ -2,45 +2,9 @@ import { default as news_20190619news1 } from "./articles/news_20190619news1.jsx
 import { default as news_20190619news2 } from "./articles/news_20190619news2.jsx";
 import { default as news_20190619news3 } from "./articles/news_20190619news3.jsx";
 import { default as news_20190619news4 } from "./articles/news_20190619news4.jsx";
-import { default as release } from "./articles/release.jsx";
+import { default as news_test } from "./articles/news_test.jsx";
 
 const news =
-[
-  {
-    title: "第2回定期公演決定のお知らせ",
-    url: "20190619news4",
-    date: "2019-06-20",
-    author: "三石ミゲール",
-    component: C20190619news4
-  },
-  {
-    title: "団員・スタッフ募集に関して",
-    url: "20190619news3",
-    date: "2019-06-20",
-    author: "三石ミゲール",
-    component: C20190619news3
-  },
-  {
-    title: "チケット予約開始（ホームページ予約第1次）",
-    url: "20190619news2",
-    date: "2019-06-19",
-    author: "三石ミゲール",
-    component: C20190619news2
-  },
-  {
-    title: "ホームページ開設のお知らせ",
-    url: "20190619news1",
-    date: "2019-06-19",
-    author: "三石ミゲール",
-    component: C20190619news1
-  },
-  {
-    title: "ホームページリリース",
-    url: "release",
-    date: "2019-05-02",
-    author: "三石ミゲール",
-    component: Crelease
-  }
-]
+
 
 export default news
