af782a167ee3ce2f0daa424cda91516650c95de6
manimigue <manimigue0701@gmail.com>
2020-04-03 00:28:28 +0900

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3180ebe5f06d448ce983aa5c9b96d2c7.js"
+  "/precache-manifest.6d5561723b6b695f612078b0b0b6eba4.js"
 );
 
 self.addEventListener('message', (event) => {
